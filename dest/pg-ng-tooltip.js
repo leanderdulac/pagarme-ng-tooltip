3751e036a34638972d247147431a5eeaabeb083a
Rafael Violato <rfviolato@gmail.com>
2015-08-04 10:11:27 -0300

    Fix wrong event deregistration

--- dest/pg-ng-tooltip.js
+++ dest/pg-ng-tooltip.js
@@ -182,9 +182,8 @@
 			$scope.hideDelay ? $scope.hideDelay = parseInt($scope.hideDelay) : $scope.hideDelay = 0;
 			$scope.hideTimeout ? $scope.hideTimeout = parseInt($scope.hideTimeout) : $scope.hideTimeout = false;
 
-			var $mouseenter = $element.on('mouseenter', showTrigger);
-			var $mouseleave = $element.on('mouseleave', hideTrigger);
-
+			$element.on('mouseenter', showTrigger);
+			$element.on('mouseleave', hideTrigger);
 			$scope.$on('$destroy', destroy);
 
 			function showTrigger(){
@@ -213,8 +212,6 @@
 			function destroy(){
 
 				$element.off('mouseenter mouseleave');
-				$mouseenter();
-				$mouseleave();
 				
 			}
 			
